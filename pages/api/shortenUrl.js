fde1fa82985df32f5cfbfd55358cea9a3cd4d4e2
ankush <webmaster.ankush@gmail.com>
2024-04-30 12:57:19 +0530

    Update shortenUrl.js

--- pages/api/shortenUrl.js
+++ pages/api/shortenUrl.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
     };
 
     try {
-        const fetchResponse = await fetch("https://www.shorturl.at/shortener.php", requestOptions);
+        const fetchResponse = await fetch("https://api-ssl.bitly.com/v4/shorten", requestOptions);
         if (!fetchResponse.ok) {
             throw new Error('Network response was not ok');
         }
