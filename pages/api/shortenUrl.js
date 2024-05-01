739c659e83ca9ffe59bafb57ca2cd9292c5d97c4
ankush <webmaster.ankush@gmail.com>
2024-05-01 08:18:24 +0530

    Update shortenUrl.js

--- pages/api/shortenUrl.js
+++ pages/api/shortenUrl.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
     };
 
     try {
-        const fetchResponse = await fetch("https://linqbin.cc/src/api/createNewEntry", requestOptions);
+        const fetchResponse = await fetch("https://api-ssl.bitly.com/v4/shorten", requestOptions);
         if (!fetchResponse.ok) {
             throw new Error('Network response was not ok');
         }
